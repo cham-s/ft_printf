07cfc983ccad792e3172df2a0af9502109728cb6
cham-s <cattouma@student.42.fr>
2016-08-05 00:09:54 +0200

    first commit

---
+++ Makefile
@@ -0,0 +1,45 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: cattouma <cattouma@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2016/06/02 16:30:48 by cattouma          #+#    #+#              #
+#    Updated: 2016/08/05 00:05:07 by cattouma         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME 		= ft_printf
+CC 			= clang
+LIB 		= libft/libft.a
+FLAGS 		= -Wall -Werror -Wextra
+INCLUDES 	= -I include -I libft/includes
+INC			= include/ft_printf.h include/dict.h
+OBJDIR		= obj
+OBJS 		= $(OBJDIR)/ft_printf.o\
+
+.PHONY: all clean fclean re
+
+VPATH = src/ft_printf:src/cd:src/dict
+
+all: $(NAME)
+
+$(NAME): $(OBJS) $(LIB)
+	$(CC) $(FLAGS) $(INCLUDES) $(OBJS) $(LIB) -o $(NAME)
+
+$(LIB):
+	make -C libft/
+
+$(OBJDIR)/%.o : %.c $(INC)
+	@mkdir -p $(OBJDIR)
+	$(CC) -c $(FLAGS) $(INCLUDES) $< -o $@
+
+clean:
+	rm -rf $(OBJDIR)
+
+fclean: clean
+	make fclean -C libft/
+	rm -f $(NAME)
+
+re: fclean all
---
+++ src/dict/dictionary2.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dictionary2.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cattouma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/02 10:33:04 by cattouma          #+#    #+#             */
+/*   Updated: 2016/03/02 10:52:20 by cattouma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "dict.h"
+
+void	dict_insert(t_dict *d, const char *key, const char *value)
+{
+	t_elt			*e;
+	unsigned long	h;
+
+	if (!key || !value)
+		return ;
+	e = (t_elt *)ft_memalloc(sizeof(t_elt));
+	if (!e)
+		return ;
+	e->key = ft_strdup(key);
+	e->value = ft_strdup(value);
+	h = hash_function(key) % d->size;
+	e->next = d->table[h];
+	d->table[h] = e;
+	d->n++;
+	if (d->n >= d->size * MAX_LOAD_FACTOR)
+		grow(d);
+}
+
+char	*dict_search(t_dict *d, const char *key)
+{
+	t_elt *e;
+
+	e = d->table[hash_function(key) % d->size];
+	while (e)
+	{
+		if (!ft_strcmp(e->key, key))
+			return (e->value);
+		e = e->next;
+	}
+	return (NULL);
+}
+
+void	dict_delete(t_dict *d, const char *key)
+{
+	t_elt	**prev;
+	t_elt	*e;
+
+	prev = &(d->table[hash_function(key) % d->size]);
+	while (*prev)
+	{
+		if (!ft_strcmp((*prev)->key, key))
+		{
+			e = *prev;
+			*prev = e->next;
+			free(e->key);
+			free(e->value);
+			free(e);
+			return ;
+		}
+		prev = &((*prev)->next);
+	}
+}
