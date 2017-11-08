7a203b67cfb427bb6867dfce24e6a5fa13664e43
cham-s <cattouma@student.42.fr>
2017-11-08 23:13:44 +0100

    updated putunicode

--- libft/ft_putunicode.c
+++ libft/ft_putunicode.c
@@ -5,25 +5,25 @@ int	ft_putunicode(const unsigned int cp)
 	int	i;
 
 	i = 0;
-	if (cp < 128)
+	if (cp < 0x0000007F)
 	{
 		ft_putchar(cp);
 		return (1);
 	}
-	else if (cp < 2048)
+	else if (cp < 0x000007FF)
 	{
 		ft_putchar(192 | (cp >> 6));
 		ft_putchar(128 | (cp & 63));
 		return(2);
 	}
-	else if (cp < 65536)
+	else if (cp < 0x0000FFFF)
 	{
 		ft_putchar(224 | (cp >> 12));
 		ft_putchar(128 | ((cp >> 6) & 63));
 		ft_putchar(128 | (cp & 63));
 		return(3);
 	}
-	else if (cp < 1114112)
+	else if (cp < 0x001FFFFF)
 	{
 		ft_putchar(240 | (cp >> 18));
 		ft_putchar(128 | ((cp >> 12) & 63));
--- main.c
+++ main.c
@@ -19,8 +19,23 @@ int main(void)
 	unsigned long l;
 	int ret = 0;
 	int ret2 = 0;
+	char values[4] = {0, 0, 0, 0};
+	unsigned int ch = (unsigned int)L'ψ'; 
 	//ret = ft_printf("{% %}");
 	//ret2 = ft_printf("%s%s%s%s%s", "1", "2", "3", "4", "5");
-	ret2 = printf("{% 2%}");
+	//ret2 = printf("{% 2%}");
 	//printf("return: %d\n", ret);
+	printf("%lc\n", L'ψ');	
+	printf("%C", L'猫');
+	printf("%C", L'δ');
+	printf("%C", L'요');
+	printf("%C", L'莨');
+	printf("%C", L'ي');
+	unsigned char c;
+	c = 0xcf;
+	write(1, &c, 1);
+	c = 0x88;
+	write(1, &c, 1);
+	/* c = 0x0a; */
+	/* write(1, &c, 1); */
 }
